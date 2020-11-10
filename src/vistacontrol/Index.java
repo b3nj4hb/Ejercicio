2197a5ebcdd73ee3b4eb6a89f7cf9c3a99c019dd
Benjamin Daniel Huamani Bailon <benjalive12@gmail.com>
2020-11-10 14:30:33 -0500

    index
    index

--- src/vistacontrol/Index.java
+++ src/vistacontrol/Index.java
@@ -1,15 +1,13 @@
 package vistacontrol;
 
 import java.util.List;
-import modelo.Curso_c1;
-import modeloDAO.CursoDAO_c1;
-import modelo.Curso_c2;
-import modeloDAO.CursoDAO_c2;
+import modelo.Biblioteca;
+import modeloDAO.BibliotecaDAO;
 import utils.Leer;
 
 public class Index {
-    
-    public static void listarCurso_c1() {
+
+    public static void listarBiblioteca() {
         CursoDAO_c1 cursoDAO_c1 = new CursoDAO_c1();
         List<Curso_c1> cursos = cursoDAO_c1.listarcurso_c1();
         System.out.println("Listado de Cursos");
@@ -28,66 +26,60 @@ public class Index {
     }
 
     public static void eliminarCurso_c1() {
-        listarCurso_c1();
+        listarBiblioteca();
         System.out.println("Ingrese el ID del curso a Eliminar");
         int id = Leer.entero();
-        CursoDAO_c1 cursoDAO_c1 = new CursoDAO_c1();
-        cursoDAO_c1.eliminarcurso_c1(id);
+        BibliotecaDAO bibliotecaDAO = new BibliotecaDAO();
+        bibliotecaDAO.eliminarbiblioteca(id);
     }
 
-    public static void editarCurso_c1() {
-        listarCurso_c1();
-        System.out.println("Ingrese el ID del curso a editar: ");
+    public static void editarBiblioteca() {
+        listarBiblioteca();
+        System.out.println("Ingrese el ID de la Biblioteca a editar: ");
         int id = Leer.entero();
         String nombre;
         String codigo;
-        String ciclo;
-        String turno;
-        int creditos;
-        int horas_semanales;
-        System.out.println("Editar Curso");
+        String direccion;
+        String telefono;
+        String estado;
+        System.out.println("Editar Biblioteca");
         System.out.print("Nombre: ");
         nombre = Leer.cadena();
         System.out.print("Codigo: ");
         codigo = Leer.cadena();
-        System.out.print("Ciclo: ");
-        ciclo = Leer.cadena();
-        System.out.print("Turno: ");
-        turno = Leer.cadena();
-        System.out.print("Creditos: ");
-        creditos = Leer.entero();
-        System.out.print("Horas Semanales: ");
-        horas_semanales = Leer.entero();
-        Curso_c1 curso = new Curso_c1(id, nombre, codigo, ciclo, turno, creditos, horas_semanales);
-        CursoDAO_c1 cursoDAO_c1 = new CursoDAO_c1();
-        cursoDAO_c1.editarcurso_c1(curso);
+        System.out.print("Direccion: ");
+        direccion = Leer.cadena();
+        System.out.print("Telefono: ");
+        telefono = Leer.cadena();
+        System.out.print("Estado: ");
+        estado = Leer.cadena();
+        Biblioteca biblioteca = new Biblioteca(id, nombre, codigo, direccion, telefono, estado);
+        BibliotecaDAO bibliotecaDAO = new BibliotecaDAO();
+        bibliotecaDAO.editarbiblioteca(biblioteca);
     }
 
-    public static void agregarCurso_c1() {
+    public static void agregarBiblioteca() {
         String nombre;
         String codigo;
-        String ciclo;
-        String turno;
-        int creditos;
-        int horas_semanales;
-        System.out.println("Editar Curso");
+        String direccion;
+        String telefono;
+        String estado;
+        System.out.println("Editar Biblioteca");
         System.out.print("Nombre: ");
         nombre = Leer.cadena();
         System.out.print("Codigo: ");
         codigo = Leer.cadena();
-        System.out.print("Ciclo: ");
-        ciclo = Leer.cadena();
-        System.out.print("Turno: ");
-        turno = Leer.cadena();
-        System.out.print("Creditos: ");
-        creditos = Leer.entero();
-        System.out.print("Horas Semanales: ");
-        horas_semanales = Leer.entero();
-        Curso_c1 curso = new Curso_c1(nombre, codigo, ciclo, turno, creditos, horas_semanales);
-        CursoDAO_c1 cursoDAO_c1 = new CursoDAO_c1();
-        cursoDAO_c1.agregarcurso_c1(curso);
+        System.out.print("Direccion: ");
+        direccion = Leer.cadena();
+        System.out.print("Telefono: ");
+        telefono = Leer.cadena();
+        System.out.print("Estado: ");
+        estado = Leer.cadena();
+        Biblioteca biblioteca = new Biblioteca(nombre, codigo, direccion, telefono, estado);
+        BibliotecaDAO bibliotecaDAO = new BibliotecaDAO();
+        bibliotecaDAO.agregarbiblioteca(biblioteca);
     }
-    
+
     public static void listarCurso_c2() {
         CursoDAO_c2 cursoDAO_c2 = new CursoDAO_c2();
         List<Curso_c2> cursos = cursoDAO_c2.listarcurso_c2();
@@ -184,7 +176,7 @@ public class Index {
         System.out.println("5. Salir");
         System.out.print("Escoja opción [1-5]: ");
     }
-    
+
     public static void menu_c2() {
         System.out.println("Menu Ciclo 2");
         System.out.println("1. Agregar Cursos");
@@ -194,9 +186,9 @@ public class Index {
         System.out.println("5. Salir");
         System.out.print("Escoja opción [1-5]: ");
     }
-    
-    public static void switch_c1(){
-    int opcion;
+
+    public static void switch_c1() {
+        int opcion;
         do {
             menu_c1();
             opcion = Leer.entero();
@@ -211,7 +203,7 @@ public class Index {
                     editarCurso_c1();
                     break;
                 case 4:
-                    listarCurso_c1();
+                    listarBiblioteca();
                     break;
                 case 5:
                     inicio();
@@ -222,11 +214,11 @@ public class Index {
                     break;
             }
         } while (opcion != 5);
-    
+
     }
-    
-    public static void switch_c2(){
-    int opcion;
+
+    public static void switch_c2() {
+        int opcion;
         do {
             menu_c2();
             opcion = Leer.entero();
@@ -252,11 +244,11 @@ public class Index {
                     break;
             }
         } while (opcion != 5);
-    
+
     }
-    
+
     public static void inicio() {
-        
+
         int opcion;
         do {
             menu();
@@ -283,5 +275,3 @@ public class Index {
         inicio();
     }
 }
-
-
